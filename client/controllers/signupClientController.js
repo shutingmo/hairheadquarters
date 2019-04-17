e93883574c0717f2c32720c47f2142b611586448
Shuting Mo <cynthia.mo.1812@gmail.com>
2019-04-17 03:35:48 -0400

    validation to signup

--- client/controllers/signupClientController.js
+++ client/controllers/signupClientController.js
@@ -1,8 +1,14 @@
 angular.module('user').controller('signupController', ['$scope','userFactory', 
     function($scope, userFactory){
         console.log("checking");
+        
+        $scope.types = ["1", "2A", "2B", "2C", "3A", "3B", "3C", "4A", "4B", "4C"];
+        $scope.por = ["Low", "Medium", "High"];
+        
         $scope.signup = function(){
             $scope.user = [];
+
+                        
 
             console.log('scope new user is ' + JSON.stringify($scope.newUser));
 
